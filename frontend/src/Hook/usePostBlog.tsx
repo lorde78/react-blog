9214274269db01503f37a1ee609e5728028196d8
lorde78 <62546853+lorde78@users.noreply.github.com>
2022-04-28 13:46:21 +0200

    axios

--- frontend/src/Hook/usePostBlog.tsx
+++ frontend/src/Hook/usePostBlog.tsx
@@ -1,22 +1,22 @@
 import {LocalBlogPost} from "../Interface/LocalBlogPost";
+import axios from "axios";
 
 export default function usePostBlog() {
     return (token: string, blog: LocalBlogPost) => {
-        return fetch('http://localhost:2345/post-blog.php', {
+        return axios('http://localhost:2345/post-blog.php', {
             method: 'POST',
-            mode: 'cors',
-            credentials: 'include',
+            withCredentials: true,
             // Ici aussi je pourrais me passer de passer le token en
             // Bearer et juste le passer par cookie (qui est envoyé
             // automatiquement et le récupérer en PHP !
             headers: {
                 Authorization: `Bearer ${token}`
             },
-            body: new URLSearchParams({
+            data: new URLSearchParams({
                 title: blog.title,
                 content: blog.content
             })
         })
-            .then(res => res.json())
+            .then(res => res.data)
     }
 }
